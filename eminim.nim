69a8c9ae4228d30552616647e34c7d2eaab7711f
Antonis <b3liever@yandex.com>
2019-11-04 09:44:48 +0200

    and done!

--- eminim.nim
+++ eminim.nim
@@ -1,4 +1,4 @@
-import macros, parsejson, streams, strutils
+import macros, parsejson, streams, strutils, tables
 
 template loadObj(parser, body) =
    eat(parser, tkCurlyLe)
@@ -94,55 +94,74 @@ template loadFloat(parser, data, dataTy) =
    else:
       raiseParseErr(parser, "float or int")
 
-proc loadAny(nodeTy, param, parser: NimNode, isRef: bool, depth: int): NimNode =
+proc loadAny(nodeTy, param, parser, genProcs: NimNode, cache: TableRef[string,
+      NimNode], isRef: bool, depth: int): NimNode =
    if depth > 150:
       error("Recursion limit reached")
    let baseTy = getTypeImpl(nodeTy)
    case baseTy.typeKind
    of ntyRef:
-      let name = genSym(nskProc, "pack")
-      let parserPar = genSym(nskParam, "p")
-      let res = ident"result"
-      result = newStmtList(newProc(name, [nodeTy, newIdentDefs(parserPar, nnkVarTy.newTree(bindSym"JsonParser"))],
-         newStmtList(newCall(bindSym"new", res), loadAny(baseTy[0], res, parserPar, true, depth + 1)),
-         pragmas = nnkPragma.newTree(ident"nimcall")),
-         newAssignment(param, newCall(name, parser)))
+      let key = nodeTy.repr
+      if cache.hasKey(key):
+         result = newAssignment(param, newCall(cache[key], parser))
+      else:
+         let res = ident"result"
+         let parserPar = genSym(nskParam, "p")
+         let name = genSym(nskProc, "pack")
+         cache[nodeTy.repr] = name
+         genProcs.add newProc(name, [nodeTy, newIdentDefs(parserPar,
+               nnkVarTy.newTree(bindSym"JsonParser"))],
+            newStmtList(newCall(bindSym"new", res), loadAny(baseTy[0], res,
+                  parserPar, genProcs, cache, true, depth + 1)),
+            pragmas = nnkPragma.newTree(ident"nimcall"))
+         result = newAssignment(param, newCall(name, parser))
    of ntyObject, ntyTuple:
       if baseTy.kind == nnkTupleConstr:
          error("Use a named tuple instead of: " & nodeTy.repr)
-      let parser2 = if isRef: parser else: genSym(nskParam, "p")
-      let res = ident"result"
-      let caseStr = nnkCaseStmt.newTree(newDotExpr(parser2, ident"a"))
-      let idents = if baseTy.kind == nnkTupleTy: baseTy else: baseTy[2]
-      for n in idents:
-         n.expectKind nnkIdentDefs
-         caseStr.add nnkOfBranch.newTree(newLit(n[0].strVal),
-            newStmtList(getAst(getFieldValue(parser2)),
-               loadAny(n[1], newDotExpr(res, n[0]), parser2, false, depth + 1)))
-      caseStr.add nnkElse.newTree(getAst(raiseWrongKey(parser2)))
-      if isRef:
-         result = newStmtList(getAst(loadObj(parser2, caseStr)))
+      let key = nodeTy.repr
+      if cache.hasKey(key):
+         result = newAssignment(param, newCall(cache[key], parser))
       else:
-         let name = genSym(nskProc, "pack")
-         result = newStmtList(newProc(name,
-               [nodeTy, newIdentDefs(parser2, nnkVarTy.newTree(bindSym"JsonParser"))],
-               getAst(loadObj(parser2, caseStr)), pragmas = nnkPragma.newTree(ident"nimcall")),
-            newAssignment(param, newCall(name, parser)))
+         let parser2 = if isRef: parser else: genSym(nskParam, "p")
+         let res = ident"result"
+         let caseStr = nnkCaseStmt.newTree(newDotExpr(parser2, ident"a"))
+         let idents = if baseTy.kind == nnkTupleTy: baseTy else: baseTy[2]
+         for n in idents:
+            n.expectKind nnkIdentDefs
+            caseStr.add nnkOfBranch.newTree(newLit(n[0].strVal),
+               newStmtList(getAst(getFieldValue(parser2)),
+                  loadAny(n[1], newDotExpr(res, n[0]), parser2, genProcs, cache,
+                        false, depth + 1)))
+         caseStr.add nnkElse.newTree(getAst(raiseWrongKey(parser2)))
+         if isRef:
+            result = newStmtList(getAst(loadObj(parser2, caseStr)))
+         else:
+            let name = genSym(nskProc, "pack")
+            cache[nodeTy.repr] = name
+            genProcs.add newProc(name,
+               [nodeTy, newIdentDefs(parser2, nnkVarTy.newTree(
+                     bindSym"JsonParser"))],
+               getAst(loadObj(parser2, caseStr)), pragmas = nnkPragma.newTree(
+                     ident"nimcall"))
+            result = newAssignment(param, newCall(name, parser))
    of ntySet, ntySequence:
       let temp = genSym(nskTemp)
       let initTemp = nnkVarSection.newTree(newIdentDefs(temp, baseTy[1]))
-      let body = loadAny(baseTy[1], temp, parser, false, depth + 1)
+      let body = loadAny(baseTy[1], temp, parser, genProcs, cache, false,
+            depth + 1)
       let addTemp = newCall(
          if baseTy.typeKind == ntySet: bindSym"incl" else: bindSym"add", param, temp)
       result = getAst(loadSeq(parser, newStmtList(initTemp, body, addTemp)))
    of ntyArray:
       let idx = genSym(nskVar, "i")
-      let body = loadAny(baseTy[2], nnkBracketExpr.newTree(param, idx), parser, false, depth + 1)
+      let body = loadAny(baseTy[2], nnkBracketExpr.newTree(param, idx), parser,
+            genProcs, cache, false, depth + 1)
       result = getAst(loadArray(parser, param, idx, body))
    of ntyRange:
-      result = loadAny(baseTy[1][1], param, parser, false, depth + 1)
+      result = loadAny(baseTy[1][1], param, parser, genProcs, cache, false,
+            depth + 1)
    of ntyDistinct:
-      result = loadAny(baseTy[0], param, parser, false, depth + 1)
+      result = loadAny(baseTy[0], param, parser, genProcs, cache, false, depth + 1)
    of ntyString:
       result = getAst(loadString(parser, param))
    of ntyChar:
@@ -174,11 +193,13 @@ macro to*(s: Stream, T: typedesc): untyped =
    let name = genSym(nskProc, "pack")
    let parser = genSym(nskVar, "p")
    let res = ident"result"
-   let body = loadAny(typeSym, res, parser, false, 0)
+   let genProcs = newStmtList()
+   let cache = newTable[string, NimNode]()
+   let body = newStmtList(genProcs, loadAny(typeSym, res, parser, genProcs,
+         cache, false, 0))
    result = nnkStmtListExpr.newTree(
       getAst(genPackProc(parser, typeSym, name, body)),
       newCall(name, s))
-   echo result.repr
 
 when isMainModule:
    # TODO:
@@ -187,10 +208,7 @@ when isMainModule:
    # - correctly compare nim identifiers
    # - add Tables, OrderedTables
    type
-      Bar = ref object
-         s: int
       Foo = ref object
          value: int
-         b, d: Bar
    let s = newStringStream("{\"value\": 1}")
    let a = s.to(Foo)
